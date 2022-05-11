fa1094855815c62678c4fdff545fd326f795992c
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-05-10 22:05:28 -0400

    Author application index to include Express and database connection

---
+++ src/index.js
@@ -0,0 +1,17 @@
+require('dotenv').config();
+
+const express = require('express');
+
+const configuration = require('./configuration/application.configuration');
+const database = require('./database/database');
+const router = require('./route/index.route');
+
+const app = express();
+
+app.use(router);
+
+app.listen(configuration.port, async () => {
+	console.log(`PodScholar listening on port ${configuration.port}.`);
+
+	await database.connect();
+});
