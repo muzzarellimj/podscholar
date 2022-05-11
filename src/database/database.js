81680aa522c67ab0fa765883bb5b56138d27f9f1
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-05-10 21:32:39 -0400

    Author Mongo database connection and disconnection functions

--- src/database/database.js
+++ src/database/database.js
@@ -1,3 +1,39 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const configuration = require('../configuration/database.configuration');
 
-module.exports = { configuration };
+const client = new MongoClient(process.env.MONGODB_CONNECTION_STRING, {
+	serverApi: ServerApiVersion.v1,
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+});
+
+let connection = null;
+
+async function connect() {
+	client.connect((error, client) => {
+		if (error) return console.log(error);
+
+		connection = client;
+
+		console.log('Database connection established.');
+	});
+}
+
+async function disconnect() {
+	client.close((error) => {
+		if (error) return console.log(error);
+
+		console.log('Database disconnected.')
+	});
+}
+
+async function get() {
+	if (connection !== null) {
+		return connection;
+	}
+
+	return connect();
+}
+
+module.exports = { configuration, connect, disconnect, get };
