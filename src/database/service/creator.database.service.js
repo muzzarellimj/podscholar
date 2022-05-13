fe23560e2d4ef36c24bf7b70a6f0c8c8b37d15cf
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-05-13 16:22:31 -0400

    Author creator database service module, primary and stage insertion functions

---
+++ src/database/service/creator.database.service.js
@@ -0,0 +1,34 @@
+const { configuration } = require('../database');
+const { primary, stage } = require('./connection.database.service');
+
+async function insertPrimaryCreator(creator) {
+	const result = await primary(configuration.primaryCreatorCollection).insertOne(creator);
+
+	if (result !== null) {
+		console.log(`A creator document was inserted in the primary database with the _id: ${result.insertedId}`);
+
+		return result.insertedId;
+
+	} else {
+		console.log('A primary creator document insertion request has failed.');
+
+		return null;
+	}
+}
+
+async function insertStageCreator(creator) {
+	const result = await stage(configuration.stageCreatorCollection).insertOne(creator);
+
+	if (result !== null) {
+		console.log(`A creator document was inserted in the stage database with the _id: ${result.insertedId}`);
+
+		return result.insertedId;
+
+	} else {
+		console.log('A stage creator document insertion request has failed.');
+
+		return null;
+	}
+}
+
+module.exports = { insertPrimaryCreator, insertStageCreator }
