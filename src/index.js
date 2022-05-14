e992430ce7b9b6d440940118de80b1aaf7d9117d
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-05-13 23:02:14 -0400

    Migrate user profile content creation to middleware function

--- src/index.js
+++ src/index.js
@@ -11,6 +11,14 @@ const app = express();
 app.use('/asset', express.static('asset'));
 app.set('view engine', 'mustache');
 
+app.use((request, response, next) => {
+	response.locals.userProfileContent = (response.locals.authenticated === true) ?
+		'<a class="nav-link" href="/register">Account</a>' :
+		`<a class="nav-link" href="/register">Register<i class="bi bi-arrow-right ms-2"></i></a>`;
+
+	next();
+});
+
 app.use(router);
 
 app.listen(configuration.port, async () => {
--- src/route/site/site.web.route.js
+++ src/route/site/site.web.route.js
@@ -3,43 +3,27 @@ const { render } = require('../../utility/mustache.utility');
 const { createFeed } = require('../../service/content/feed.service');
 
 router.get('/', (request, response) => {
-	const userProfileContent = (response.locals.authenticated === true) ?
-		'<a class="nav-link" href="/register">Account</a>' :
-		`<a class="nav-link" href="/register">Register<i class="bi bi-arrow-right ms-2"></i></a>`;
-
 	response.status(200).send(render('src/route/site/view/index.html', {
-		userProfileContent: userProfileContent,
+		userProfileContent: response.locals.userProfileContent,
 		recentFeed: createFeed('recent', null, 'A glance at the most recent content published to PodScholar.')
 	}));
 });
 
 router.get('/faq', (request, response) => {
-	const userProfileContent = (response.locals.authenticated === true) ?
-		'<a class="nav-link" href="/register">Account</a>' :
-		`<a class="nav-link" href="/register">Register<i class="bi bi-arrow-right ms-2"></i></a>`;
-
 	response.status(200).send(render('src/route/site/view/faq.html', {
-		userProfileContent: userProfileContent
+		userProfileContent: response.locals.userProfileContent
 	}));
 });
 
 router.get('/about', (request, response) => {
-	const userProfileContent = (response.locals.authenticated === true) ?
-		'<a class="nav-link" href="/register">Account</a>' :
-		`<a class="nav-link" href="/register">Register<i class="bi bi-arrow-right ms-2"></i></a>`;
-
 	response.status(200).send(render('src/route/site/view/about.html', {
-		userProfileContent: userProfileContent
+		userProfileContent: response.locals.userProfileContent
 	}));
 });
 
 router.get('/contact', (request, response) => {
-	const userProfileContent = (response.locals.authenticated === true) ?
-		'<a class="nav-link" href="/register">Account</a>' :
-		`<a class="nav-link" href="/register">Register<i class="bi bi-arrow-right ms-2"></i></a>`;
-
 	response.status(200).send(render('src/route/site/view/contact.html', {
-		userProfileContent: userProfileContent
+		userProfileContent: response.locals.userProfileContent
 	}));
 });
 
