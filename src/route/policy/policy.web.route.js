c103153251b3872500f0f730a8747a439aee0ee1
Michael Muzzarelli <muzzarellimj@outlook.com>
2022-05-13 23:11:25 -0400

    Author Privacy Policy view document, render in respective web route

--- src/route/policy/policy.web.route.js
+++ src/route/policy/policy.web.route.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
+const { render } = require("../../utility/mustache.utility");
 
 router.get('/policy/privacy', (request, response) => {
-	// TODO: response: privacy policy
+	response.status(200).send(render('src/route/policy/view/privacy.html', {
+		userProfileContent: response.locals.userProfileContent
+	}));
 });
 
 router.get('/policy/use', (request, response) => {
